d6a1c8789bd857f3d32c4948fe2a41427b2759a7
ZSoJes <GomezSoteloJuan@Gmail.com>
2016-11-08 11:45:29 -0600

    boton buscar funcionando

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -3,48 +3,43 @@
 # Crear un post nuevo
 # Editar un post existente
 # Eliminar un post existente
-get '/post' do
-  redirect to '/'
-end
 
 post '/post' do
   titulo = params["post"]["title"]
   post = params["post"]["message"]
   tag = params["post"]["tags"]
 
-# if Post.create(titulo: titulo, mensaje: post).valid?
   post_now = Post.create(titulo: titulo, mensaje: post)
 
   lista = tag.split(",")                        # separar tags en un arreglo
     lista.each do |t|                           # recorrer lista
       t = t.rstrip.lstrip                         # quitar espacios
-      tag_creado = Tag.create(etiquetas: t)       # creando registro en la tabla tags
+      t.downcase!
+      
+      dato = Tag.find_by(etiquetas: t)
+
+      if dato.nil?                                  # si no existe crealo
+        tag_creado = Tag.create(etiquetas: t)       # creando registro en la tabla tags
+      else                                          # si existe buscalo
+        tag_creado = Tag.find_by(etiquetas: t)
+      end
+
       post_now.blogs.create(tag: tag_creado)      # crear registro en la tabla blog de ambos id
       post_now.tags                               #=> <ActiveRecord::Associations::CollectionProxy [#<id: 1, post id: 1, blog id: 1, created_at: "2016-01-25 18:45:00", updated_at: "2016-01-25 18:45:00"]>
     end
-  # end
 
   redirect to '/'
-  # erb :index
 end
 
 get '/show_all' do
   @blog = Post.all
-  puts @blog
-  # where
-  erb :show_all
-end
-
-post '/show_all' do
-  @blog = Post.all
-  puts @blog
-  erb :show_all
+  if @blog.empty?
+    erb :index
+  else
+    erb :show_all
+  end
 end
 
 get '/find' do
-  erb :find_edit
+  erb :find
 end
-
-# post '/find' do
-#   erb :find_edit
-# end
--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,1 +1,26 @@
 # Mostrar todos los posts con cierto Tag.
+before "/find" do 
+  if params[:error] == 'true'
+    @error = "No dejes el campo vacio"
+  end
+end
+
+post '/tag' do
+@post_resultado = []
+
+  tag_text = params[:tag_find]
+puts tag_text
+puts tag_text.nil?
+  unless tag_text.empty?
+    tag_text.downcase!
+    dato = Tag.find_by(etiquetas: tag_text)
+    @post_find = Blog.where(tag_id: dato.id)
+
+    @post_find.each do |t| 
+      @post_resultado << Post.where(id: t.post_id) 
+    end
+  erb :find
+  else
+  redirect to '/find?error=true'  
+  end
+end
